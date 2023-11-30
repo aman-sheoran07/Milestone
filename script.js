7f40b2b76fc71e67f28b8d187896ae77a2fe2e78
Aman sheoran <90210280+aman-sheoran07@users.noreply.github.com>
2023-11-30 21:43:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Any JavaScript functionality can be added here if needed
+// For example, smooth scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
